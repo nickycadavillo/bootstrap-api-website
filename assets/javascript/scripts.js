8b873bbfb973e24226ff574b7f64dd1c13af2328
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-03-02 18:57:51 -0500

    first api finished

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,1 +1,23 @@
 
+$(function(){
+   'use strict';
+
+//tabs
+$('#restaurants').on('click', function (){
+  window.location = "restaurants.html";
+});
+
+$('#home').on('click', function (){
+  window.location = "index.html";
+});
+
+
+//YUMMLY APP KEY: 55ca31c7ecf4406e6da2b957415f4c3d
+//YUMMLY APP ID: 9a7dc1b9
+
+$.getJSON("http://api.yummly.com/v1/api/recipes?_app_id=9a7dc1b9&_app_key=55ca31c7ecf4406e6da2b957415f4c3d&greek"){
+
+}
+
+
+)};
